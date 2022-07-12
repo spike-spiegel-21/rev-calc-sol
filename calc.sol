4a30011db6c35f34eba5ee16f9bf1f73a749dc91
spike-spiegel-21 <bizmiku@gmail.com>
2022-07-12 15:14:56 +0530

    storage and retrive added

--- calc.sol
+++ calc.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Math {
     //storing in int for negative operations
+    int256 store;
     
     //division
     function division(int256 a, int256 b) public payable returns (int256) {
@@ -31,4 +32,7 @@ contract Math {
         return store;
     }
     
+    function retrieve() public view returns (int256){
+        return store;
+    }
 }
