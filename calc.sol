8fe4d27564f63a77f0acebec262224f4bdd5ff22
spike-spiegel-21 <bizmiku@gmail.com>
2022-07-12 16:55:39 +0530

    function made external

--- calc.sol
+++ calc.sol
@@ -3,23 +3,23 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Math {
     //storing in int for negative operations
-    int256 store;
-    
+
+    int256 public store;
+
     //division
-    function division(int256 a, int256 b) public payable returns (int256) {
+    function division(int256 a, int256 b) external returns (int256) {
         store = a/b;
         return store;
     }
 
     //multiply
-    function multiply(int256 a, int256 b) public payable returns (int256) {
+    function multiply(int256 a, int256 b) external returns (int256) {
         store = a*b;
         return store;
     }
     
-    
     //subtraction
-    function subtract(int256 a, int256 b) public payable returns (int256){
+    function subtract(int256 a, int256 b) external returns (int256){
         if(a>b) store = a-b;
         else store = b-a;
 
@@ -27,11 +27,11 @@ contract Math {
     }
     
     //addition
-    function add(int256 a, int256 b) public payable returns (int256) {
+    function add(int256 a, int256 b) external returns (int256) {
         store = a+b;
         return store;
     }
-    
+
     function retrieve() public view returns (int256){
         return store;
     }
